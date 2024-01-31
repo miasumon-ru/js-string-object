9e927517dc68c19ef4ba9ab8a2afdedc78d0eba1
miasumonru <miasumon.ru@gmail.com>
2024-01-31 18:01:00 +0600

    js string and object discussion

---
+++ string-vs-array.js
@@ -0,0 +1,35 @@
+const country = 'Bangladesh';
+const division = 'Rangpur';
+const district = 'Gaibandha';
+// const thana = new String('Sundarganj');
+
+// console.log(country);
+// console.log(division);
+// console.log(district);
+// console.log(thana);
+
+
+// const numbers = [14, 25, 45, 63, 35, 28];
+// console.log(numbers.length);
+
+// numbers[1]= 987;  In this way , we can assign an element of an array by a index number
+// console.log(numbers);
+// console.log(numbers[0]);
+// console.log(numbers[1]);
+
+const capital = "Dhaka  ";
+// console.log(capital.length);  here length of the string identifies how many spaces of the value of capital have been taken.
+
+// capital[0]= 'F'; but here not change the letter like an array doese because a string is not changeable, other words , immutable
+console.log(capital);
+console.log(capital[4]);
+
+// summary 
+
+// 1) string is immutable (not changeable)
+// 2) array is mutable ( changeable)
+
+
+
+
+

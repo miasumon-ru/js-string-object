fb77f6ecff6d8443026f9decd349e443c0856d38
miasumonru <miasumon.ru@gmail.com>
2024-01-31 18:52:19 +0600

    slice topic discussion

---
+++ slice.js
@@ -0,0 +1,20 @@
+// const address = 'andorkilla bandorban';
+// const part = address.slice(2,5);
+// here the parameter 2 indicates the starting index number and the parameter 5 indicates the end before index
+// console.log(part);
+
+// const student = 'I am a good and hardworking person';
+// console.log(student.split(''));
+// console.log(student.split(' '));
+// console.log(student.split('g'));
+// Split a string into substrings using the specified separator and return them as an array.
+
+
+// const friendStr = 'Rahim, Kahim, Dahim, Lahim';
+// const friends = friendStr.split(',');
+// console.log(friends);
+
+const realFriend = [ 'Rahim', ' Kahim', ' Dahim', ' Lahim' ];
+console.log(realFriend.join('|'));
+console.log(realFriend.join('_'));
+console.log(realFriend.join(','));
